b1bbeded60a963466fce93bd27f5155b7d0df72f
Nahuzacarias <zacarias.nahu@gmail.com>
2023-02-19 22:24:37 -0300

    versioncasifinal

--- js/index.js
+++ js/index.js
@@ -4,12 +4,14 @@ let cartas = document.getElementById("carta");
 let ocultartienda = document.getElementById("ocultartienda3")
 ocultartienda.addEventListener("click", ocultartienda2)
 
+
 let carrito = []
 
 
 class Inmueble {
-    constructor(nombre, ubicacion, ambientes, precio, propiedad, foto, descripcion) {
+    constructor(id, nombre, ubicacion, ambientes, precio, propiedad, foto, descripcion) {
 
+        this.id = id;
         this.nombre = nombre;
         this.ubicacion = ubicacion;
         this.ambientes = ambientes;
@@ -21,28 +23,22 @@ class Inmueble {
     }
 }
 
-
-/*const casa230m = new Inmueble("casa230m", "Recoleta", "4", 210000, "casa", "casa230m.jpg", "Casa de 4 ambientes en uno de los mejores barrios de CABA.");
-const smallterreno = new Inmueble("smallterreno", "neuquen", "0", 160000, "terreno", "smallterreno.jpg", "Terreno ubicado en Neuquén cerca del puerto con una vista increíble al río.");
-const mansion200m = new Inmueble("mansion200m", "olivos", "6", 300000, "mansion", "mansion200mjpg.jpg", "Exclusiva mansión en pleno corazón de CABA.");
-const departamentoXX = new Inmueble("departamentoXX", "merlo", "2", 100000, "departamento", "departamentoXX.jpg", "Departamento de 2 ambientes en Buenos Aires.");
-const casa215m = new Inmueble("mansion215m", "Palermo", "3", 150000, "casa", "casa215m.jpg", "Casa de 3 ambientes amueblado en zona de Palermo.")*/
-
 const cargarinmuebles = async () => {
-
     const response = await fetch("inmuebles.json")
     const data = await response.json()
-    console.log(data)
+
     for (let inmueble of data) {
 
-let nuevoinmuebles = new Inmueble (inmueble.nombre,inmueble.ubicacion,inmueble.ambientes,inmueble.precio,inmueble.propiedad,inmueble.foto,inmueble.descripcion)
-propiedades.push(nuevoinmuebles)
-console.log(nuevoinmuebles)
+        let nuevoinmuebles = new Inmueble(inmueble.id, inmueble.nombre, inmueble.ubicacion, inmueble.ambientes, inmueble.precio, inmueble.propiedad, inmueble.foto, inmueble.descripcion)
+        propiedades.push(nuevoinmuebles)
+
+        mostrarinmuebles()
+
     }
 
 }
 
-cargarinmuebles()
+
 
 const propiedades = []
 
@@ -51,26 +47,52 @@ const propiedades = []
 
 function mostrarinmuebles() {
 
-    
+
     cartas.innerHTML = ``
     for (let inmueble of propiedades) {
-
+        const { id, nombre, ubicacion, ambientes, precio, propiedad, foto, descripcion } = inmueble
         let nuevoinmueble = document.createElement("div")
         nuevoinmueble.classList.add("col-12", "col-md-6", "col-lg-4", "my-3")
 
         nuevoinmueble.innerHTML = `<div class="card" style="width: 18rem">
-    <img class="cuadrado" src="../assets/${inmueble.foto}" alt="${inmueble.nombre}">
+    <img class="cuadrado" src="../assets/${foto}" alt="${nombre}">
 <div class="card-body">
-<h5 class="card-title">${inmueble.nombre}</h5>
-    <p class="card-text">${inmueble.descripcion}</p>
-    <button onclick="agregarproducto(${inmueble.nombre})"  class="btn btn-primary">Agregar al carrito</button>
+<h5 class="card-title">${nombre}</h5>
+    <p class="card-text">${descripcion}</p>
+    <p class="card-text">${precio}US$</p>
+    <button id="comprar${id}"class="btn btn-primary">Agregar al carrito</button>
+    
 </div>`;
-
         cartas.appendChild(nuevoinmueble)
 
+        const btnComprar = document.getElementById(`comprar${id}`)
+        btnComprar.addEventListener("click", () => {
+            let modal = document.getElementById("modal-body");
+            let nuevocarro = document.createElement("div");
+            nuevocarro.innerHTML = `<div>
+            
+            <div class="modal-contenedor">
+            <div>
+            <img class="img-fluid img-carrito" src="../assets/${foto}"/>
+            </div>
+            <div>
+            <p>Producto: ${nombre}</p>
+            <p>Precio: ${precio}</p>
+            <p>Ambientes :${ambientes}</p>
+            <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+            </div>
+            </div>
+            </div>`
+
+            modal.appendChild(nuevocarro)
+        })
+    };
+}
 
 
-    }
+function agregarproducto(nombre) {
+
+    console.log(nombre)
 }
 
 
@@ -85,42 +107,5 @@ mostrartienda.onclick = () => {
     mostrarinmuebles()
 }
 
-mostrarinmuebles()
-
-
-/*function agregarproducto(inmu) {
-
-
-
-    const item = propiedades.find((inmueble) => inmueble.nombre === inmueble.nombre)
-    carrito.push(item)
-    mostrarcarrito()
-}
-
-
-
-const mostrarcarrito = () => {
-
-    const modalbody = document.querySelector('.modal .modal-body')
-
-    carrito.forEach((inmueble) => {
-
-
-        modalbody.innerHTML = `
-    <div class= "modal-contenedor">
-    <div>
-    <img class="img-fluid img-carrito" src="../assets/${inmueble.foto}"/>
-    </div>
-    <p>Producto: ${inmueble.nombre}<p>
-    <p>ubicacion: ${inmueble.ubicacion}<p>
-    <p>ambientes: ${inmueble.ambientes}<p>
-    <p>precio: ${inmueble.precio}<p>
-    <p>propiedad: ${inmueble.propiedad}<p>
-    <p>descripcion: ${inmueble.descripcion}<p>
-    <button class="btn btn-danger" >Eliminar producto </button>
-    </div>
-    `
-    })
-
-}*/
+cargarinmuebles()
 
